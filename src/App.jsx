8a43656d2908cf64ffac33ad24ac196915144738
Aditya-Devarkonda <123838871+Aditya-Devarkonda@users.noreply.github.com>
2024-02-26 03:45:08 +0530

    building project - step 4
    all features except prev & next step in Main area are completed.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export default function Board() {
 
   const [squareValues, setSquarevalues] = React.useState(Array(9).fill(null));
   const [currentMove, setCurrentMove] = React.useState(true) ;
+  const [winnerName,setWinnerName] = React.useState("Let's Start");
 
   function toggleMove(currentMove){
     setCurrentMove(prevmove=> !prevmove)
@@ -16,11 +17,23 @@ export default function Board() {
     {
       return
     }
+
     const tempArray = squareValues.slice();
     tempArray[squareNumber]= toggleMove(currentMove)
     setSquarevalues(tempArray)
+
+    const winner = calculateWinner(tempArray);
+    if (winner) {
+      setWinnerName(`${winner} is a winner`);
+    } else {
+      if (!tempArray.includes(null)) {
+        setWinnerName("No winner");
+      } else {
+        setWinnerName(`Next move: ${currentMove ? 'O' : 'X'}`);
+      }
+    }
   }
-  
+
   function calculateWinner(squareLines){
     const winningLines = [
       [0,1,2],
@@ -43,23 +56,37 @@ export default function Board() {
 
   function ResetSquareValues(){
     setSquarevalues(Array(9).fill(null))
+    setWinnerName("Let's Start");
   }
 
   console.log(squareValues);
 
-  return (
+  return(
     <div className="game-layout">
-      <div className="board-table">
-        {squareValues.map((value, index) => (
-          <Square
-            key={index}
-            value={value}
-            clas={`square s${index + 1}`}
-            toggleFunction={() => toggleSquareValue(index)}
-          />
-        ))}
+      <div className="left-area">
+
       </div>
-      <button onClick={ResetSquareValues} className="reset-button">Reset Board</button>
+      <div className="main-area">
+        <div className="winner-announcement">{winnerName}</div>
+        <div className="board-table">
+          {squareValues.map((value, index) => (
+            <Square
+              key={index}
+              value={value}
+              clas={`square s${index + 1}`}
+              toggleFunction={() => toggleSquareValue(index)}
+            />
+          ))}
+        </div>
+        <div className="All-buttons">
+          <button className="previous round buttons">⬅ Prev step</button>
+          <button onClick={ResetSquareValues} className="reset buttons">Reset Board</button>
+          <button className="next round buttons">Next step ⮕</button>
+        </div>
+      </div>
+      <div className="right-area">
+
+      </div>
     </div>
   )
 }
