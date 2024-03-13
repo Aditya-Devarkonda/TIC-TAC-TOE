f70aa955ce4639bed3b0a05a464da4721cb9bbeb
Aditya-Devarkonda <123838871+Aditya-Devarkonda@users.noreply.github.com>
2024-03-13 21:11:55 +0530

    Refactor: Player score names added

--- src/components/RightArea.jsx
+++ src/components/RightArea.jsx
@@ -3,15 +3,16 @@
 import React from 'react';
 
 export default function ScoreCard ({playerXName, playerOName, scoreX, scoreO}){
-
+    const playerX ="Player X Score"
+    const playerO = "Player O Score"
     return (
       <div className="score-card">
         <div className="player-card">
-          <h2 className='player-name'>{playerXName}</h2>
+          <h2 className='player-name'>{playerXName||playerX}</h2>
           <div className="score">{scoreX}</div>
         </div>
         <div className="player-card">
-          <h2 className='player-name'>{playerOName}</h2>
+          <h2 className='player-name'>{playerOName||playerO}</h2>
           <div className="score">{scoreO}</div>
         </div>
       </div>
